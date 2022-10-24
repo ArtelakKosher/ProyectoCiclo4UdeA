a2131dccef0cefe25ae5d1f9c3dbc7fdbec531b6
MauricioV <mvargas.rodriguez@gmail.com>
2022-10-24 13:44:39 -0500

    Cambios en Navbar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import TopNavbar from "./navbar/TopNavbar";
 import Navbar from "./navbar/Navbar";
-import BottomNavbar from "./navbar/BottomNavbar";
 
 const Header = () => {
   return (
     <div>
-      <TopNavbar />
       <Navbar />
-      <BottomNavbar />
     </div>
   );
 };
--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,49 +1,119 @@
-import React from "react";
-import "../../../assets/styles/navbar.css";
-import "../../../assets/styles/main.css";
-import { NavLink} from "react-router-dom";
+import {
+  IconContainer,
+  IconLogoMobile,
+  IconNavLink,
+  ImgLogo,
+  Logo,
+  Menu,
+  MenuItem,
+  MenuItemLink,
+  MenuNavLink,
+  NavbarContainer,
+  NavbarWrapper,
+  Search,
+  SearchBar,
+  SearchBarButton,
+  SearchBarInput,
+  SearchContainer,
+} from "./Navbar.elements";
 
-const nav__links = [
-  {
-    display: "Inicio",
-    path: "/home",
-  },
-  {
-    display: "Productos",
-    path: "/products",
-  },
-  {
-    display: "Carrito",
-    path: "/cart",
-  },
-  {
-    display: "Contacto",
-    path: "/contact",
-  },
-];
+import imgLogo from "../../../assets/images/logos/artelak20cm.png";
+
+import {
+  FaSearch,
+  FaUser,
+  FaHeart,
+  FaShoppingCart,
+  FaBars,
+  FaTimesCircle,
+  FaHome,
+} from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const ChangeClick = () => {
+    setClick(!click);
+  };
+
   return (
-      <div className="nav__container">
-        <div className="nav__wrapper">
-          {/* ======= menu ======= */}
-          <div className="navigation">
-            <div className="menu">
-              {nav__links.map((item, index) => (
-                <NavLink
-                  to={item.path}
-                  key={index}
-                  className={(navClass) =>
-                    navClass.isActive ? "active__menu" : ""
-                  }
-                >
-                  {item.display}
-                </NavLink>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+    <>
+      <NavbarContainer>
+        <NavbarWrapper>
+          <Logo>
+            <ImgLogo src={imgLogo} alt="Logo Artelak" />
+          </Logo>
+
+          <Search>
+            <SearchBar>
+              <SearchBarInput
+                type="text"
+                placeholder="Buscar productos (e.j. queso)"
+              />
+              <SearchBarButton type="submit">
+                <FaSearch size={25} />
+              </SearchBarButton>
+            </SearchBar>
+          </Search>
+
+          <IconContainer>
+            <IconNavLink to="/home">
+              <FaHome size={25} />
+            </IconNavLink>
+
+            <IconNavLink to="/login">
+              <FaUser size={25} />
+            </IconNavLink>
+
+            <IconNavLink to="/favorites">
+              <FaHeart size={25} />
+            </IconNavLink>
+
+            <IconNavLink to="/cart">
+              <FaShoppingCart size={25} />
+            </IconNavLink>
+
+            <IconLogoMobile onClick={() => ChangeClick()}>
+              {click ? <FaTimesCircle size={25} style={{color:"var(--artelak-ocre)"}}/> : <FaBars size={25} />}
+            </IconLogoMobile>
+          </IconContainer>
+
+          <Menu click={click}>
+            <MenuNavLink to="/home">
+              <MenuItem onClick={() => ChangeClick()}>INICIO</MenuItem>
+            </MenuNavLink>
+            <MenuNavLink to="/products">
+              <MenuItem onClick={() => ChangeClick()}>PRODUCTOS</MenuItem>
+            </MenuNavLink>
+            <MenuNavLink to="/login">
+              <MenuItem onClick={() => ChangeClick()}>INGRESAR</MenuItem>
+            </MenuNavLink>
+            <MenuNavLink to="/favorites">
+              <MenuItem onClick={() => ChangeClick()}>FAVORITOS</MenuItem>
+            </MenuNavLink>
+            <MenuNavLink to="/cart">
+              <MenuItem onClick={() => ChangeClick()}>CARRITO</MenuItem>
+            </MenuNavLink>
+            <MenuNavLink to="/contact">
+              <MenuItem onClick={() => ChangeClick()}>CONTÁCTO</MenuItem>
+            </MenuNavLink>
+          </Menu>
+        </NavbarWrapper>
+      </NavbarContainer>
+
+      <SearchContainer>
+        <SearchBar>
+          <SearchBarInput
+            type="text"
+            placeholder="Buscar productos (e.j. queso)"
+          />
+          <SearchBarButton type="submit">
+            <FaSearch size={25} />
+          </SearchBarButton>
+        </SearchBar>
+      </SearchContainer>
+    </>
   );
 };
 
