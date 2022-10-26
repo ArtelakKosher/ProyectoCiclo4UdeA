7e0bce75ec6474eed131aba12f1d14929c789e1c
MauricioV <mvargas.rodriguez@gmail.com>
2022-10-26 18:11:39 -0500

    Ajustes de estilos y contenido

---
+++ src/components/products/Product.elements.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { Container, Row, Col } from "reactstrap";
+
+export const ProductContainer = styled.div`
+  margin: 40px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const ProductContainerRow= styled(Row)`
+    display: flex;
+`;
+
+export const ProductContainerColumn = styled(Col)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
