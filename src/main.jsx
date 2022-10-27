b0e29571d37af15773c1de3f3d903b6726efd9dd
MauricioV <mvargas.rodriguez@gmail.com>
2022-10-27 15:15:52 -0500

    Ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,17 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-import { StyledEngineProvider } from "@mui/material/styles";
 
 const options = {
   timeout: 3000,
   position: positions.BOTTOM_CENTER,
-  transition: transitions.SCALE,
-};
+  transition: transitions.SCALE
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <StyledEngineProvider injectFirst>
-      <AlertProvider template={AlertTemplate} {...options}>
-        <App />
-      </AlertProvider>
-    </StyledEngineProvider>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </Provider>
 );
