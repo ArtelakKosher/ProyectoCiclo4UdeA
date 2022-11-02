dc5587041083baf455aae16a48edc62b6790d710
MauricioV <mvargas.rodriguez@gmail.com>
2022-11-02 16:03:26 -0500

    Ajustes en tarjeta de producto

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,65 +1,35 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/joy/Typography";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCardImage,
+  CCardSubtitle,
+  CCardText,
+  CCardTitle,
+  CContainer,
+  CListGroup,
+  CListGroupItem,
+} from "@coreui/bootstrap-react";
+import { IconButton, Rating } from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
-import Rating from "@mui/material/Rating";
-import { styled } from "@mui/system";
-import { makeStyles } from "@mui/styles";
-import Box from "@mui/material/Box";
 import CurrencyFormat from "react-currency-format";
-import { Flag } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
+
+import Button from "@mui/material/Button";
+import { styled } from "@mui/system";
 import { Link } from "react-router-dom";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 350,
-    marginTop: 4,
-  },
-  leftAlignItem: {
-    marginRight: "auto",
-  },
-  rightAlignItem: {
-    marginLeft: "auto",
-  },
-  parentFlexLeft: {
-    display: "flex",
-    justifyContent: "flex-start",
-  },
-  parentFlexRight: {
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-  parentFlexSplit: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  blue: {
-    color: "#12284c",
-  },
-}));
-
-const StyledShareIcon = styled(ShareIcon, {
-  name: "StyledShareIcon",
-  slot: "Wrapper",
-})({
-  color: "#12284c",
-  "&:hover": { color: "#cc9e6a" },
-});
 
 const StyledButton = styled(Button, {
   name: "StyledButton",
   slot: "Wrapper",
 })({
-  color: "#12284c",
-  "&:hover": { color: "#cc9e6a" },
+  background:"#12284c",
+  color: "white",
+  "&:hover": { color: "#12284c", background:"#cc9e6a"},
 });
+
 
 const ProductCard = ({
   id,
@@ -81,101 +51,61 @@ const ProductCard = ({
   qualifications,
   reviews,
   createdAt,
-  updatedAt
+  updatedAt,
 }) => {
-  const classes = useStyles();
-
-  const [value, setValue] = React.useState(2);
-
   return (
-    <Box
-      key={sku}
-      m={0}
-      pt={0}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Card sx={{ width: 280, height: 500 }}>
-        <CardHeader
-          action={
-            <IconButton
-              aria-label="Compartir"
-              className={classes.rightAlignItem}
-            >
-              <StyledShareIcon />
+    <>
+      <CCard className="h-100">
+        <CCardHeader className="d-flex flex-row align-items-center">
+          <CContainer className="w-auto text-start">
+            <CCardTitle className="h5">{title}</CCardTitle>
+            <CCardSubtitle className="mb-1 text-muted">
+              {subtitle}
+            </CCardSubtitle>
+          </CContainer>
+          <CContainer className="w-auto text-end">
+            <IconButton aria-label="share">
+              <ShareIcon />
             </IconButton>
-          }
-          title={title}
-          subheader={subtitle}
-          className={classes.blue}
-        />
-        <CardMedia component="img" height="200" image={image} alt={title} />
-        <CardContent>
-          <Typography
-            variant="body2"
-            className={classes.blue}
-            component="span"
-            style={{
-              textAlign: "left",
-              color: "#12284C",
-              fontFamily: "Nunito Sans",
-            }}
-          >
-            Descripción: {description}
-            <br />
-            <br />
-            <Box display="flex" justifyContent="flex-end">
-              <div>
-                <Typography
-                  level="body"
-                  component="strong"
-                  style={{
-                    color: "#12284C",
-                    fontFamily: "Nunito Sans",
-                  }}
-                >
-                  Precio:{" "}
-                </Typography>
-                <Typography
-                  mb={2}
-                  maxWidth={400}
-                  style={{
-                    color: "#12284C",
-                    fontFamily: "Nunito Sans",
-                  }}
-                >
-                  <CurrencyFormat
-                    value={price}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    prefix={"$"}
-                  />
-                </Typography>
-              </div>
-            </Box>
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <Rating
-            name="read-only"
-            value={rating}
-            precision={0.1}
-            readOnly
-            className={classes.leftAlignItem}
-          />
-          <Link to={`/product/${id}`}>
+          </CContainer>
+        </CCardHeader>
+        <CCardImage orientation="top" src={image} />
+        <CListGroup flush>
+          <CListGroupItem>
+            <Rating
+              name="rating"
+              defaultValue={rating}
+              precision={0.1}
+              readOnly
+            />
+          </CListGroupItem>
+        </CListGroup>
+        <CCardBody>
+          <CCardText>{description}</CCardText>
+          <CCardText className="text-end">
+            <span>
+              <b>Precio: </b>
+              <CurrencyFormat
+                value={price}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+              />
+            </span>
+          </CCardText>
+        </CCardBody>
+        <CCardFooter className="text-medium-emphasis text-center">
+          <Link to={`/product/${id}`} style={{textDecoration:"none"}}>
             <StyledButton
               size="small"
               color="primary"
-              className={classes.leftAlignItem}
             >
               Ver detalles
             </StyledButton>
           </Link>
-        </CardActions>
-      </Card>
-    </Box>
+        </CCardFooter>
+      </CCard>
+    </>
   );
 };
 
--- src/components/products/ProductsGrid.jsx
+++ src/components/products/ProductsGrid.jsx
@@ -1,8 +1,7 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ProductCard from "./ProductCard";
-import Box from "@mui/material/Box";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import { CCol, CContainer, CRow } from "@coreui/bootstrap-react";
 import { Typography } from "@mui/material";
 
 import { useEffect } from "react";
@@ -11,12 +10,6 @@ import { getProducts } from "../../actions/productActions";
 import { useAlert } from "react-alert";
 
 import LinearProgress from "@mui/material/LinearProgress";
-
-const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
-  textAlign: "center",
-  boxShadow: "none",
-}));
 
 const ProductsGrid = () => {
   const { loading, products, error } = useSelector((state) => state.products);
@@ -33,60 +26,57 @@ const ProductsGrid = () => {
     alert.success("Productos cargados");
   }, [dispatch]);
 
-  console.log(products)
+  console.log(products);
 
   return (
     <>
       {loading ? (
         <LinearProgress />
       ) : (
-        <div className="products-grid">
-          <Box m={5}>
-            <Box
-              m={0}
-              pt={0}
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Typography
-                component="strong"
-                mb={4}
-                variant="h2"
-                align="center"
-                style={{
-                  color: "#12284C",
-                  paddingTop: "0px",
-                  fontFamily: "Nunito Sans",
-                }}
-              >
-                Productos
-              </Typography>
-            </Box>
-            <Grid container spacing={6}>
-              {products.map((ProductData) => (
-                <Grid item key={ProductData.id} xl={3} lg={4} md={6} sm={6}>
-                  <Item>
-                    <ProductCard
-                      id={ProductData._id}
-                      sku={ProductData.sku}
-                      title={ProductData.title}
-                      subtitle={ProductData.subtitle}
-                      image={ProductData.image}
-                      category={ProductData.category}
-                      description={ProductData.description}
-                      ingredients={ProductData.ingredients}
-                      price={ProductData.price}
-                      rating={ProductData.rating}
-                      timeLeft={ProductData.timeLeft}
-                      totalSales={ProductData.totalSales}
-                    />
-                  </Item>
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-        </div>
+        <CContainer
+          fluid
+          className="mt-4 mb-4 d-flex flex-column justify-content-center align-items-center"
+        >
+          <Typography
+            component="strong"
+            variant="h2"
+            align="center"
+            style={{
+              color: "#12284C",
+              paddingTop: "0px",
+              fontFamily: "Nunito Sans",
+            }}
+          >
+            Productos
+          </Typography>
+          <CRow
+            className="m-3"
+            xs={{ cols: 1, gutter: 4 }}
+            sm={{ cols: 2 }}
+            md={{ cols: 3 }}
+            xl={{ cols: 4 }}
+            xxl={{ cols: 5 }}
+          >
+            {products.map((ProductData) => (
+              <CCol xs key={ProductData._id}>
+                <ProductCard
+                  id={ProductData._id}
+                  sku={ProductData.sku}
+                  title={ProductData.title}
+                  subtitle={ProductData.subtitle}
+                  image={ProductData.image}
+                  category={ProductData.category}
+                  description={ProductData.description}
+                  ingredients={ProductData.ingredients}
+                  price={ProductData.price}
+                  rating={ProductData.rating}
+                  timeLeft={ProductData.timeLeft}
+                  totalSales={ProductData.totalSales}
+                />
+              </CCol>
+            ))}
+          </CRow>
+        </CContainer>
       )}
     </>
   );
