3749b827e890549b4acf61c081b4733dab984a7e
MAlexVR <mvargas.rodriguez@gmail.com>
2022-11-05 11:49:27 -0500

    Delete src directory

--- src/components/UI/Cart.element.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const CartContainer = styled.div`
-  margin: 40px;
-  display: flex;
-  align-content: center;
-  justify-content: center;
-  flex-direction: column;
-`;
--- src/components/layout/Content.jsx
+++
@@ -1,8 +0,0 @@
-import { Box } from "./Box";
-import Routes from "../../routes/Routers";
-
-export const Content = () => (
-  <Box css={{ px: "$12", mt: "$8", "@xsMax": { px: "$10" } }}>
-    <Routes />
-  </Box>
-);
